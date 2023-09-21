1bf3564cc0f0663ef8b6696174a01742158cd4b9
sultanchik1serik <142357734+sultanchik1serik@users.noreply.github.com>
2023-09-21 19:21:45 -0400

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,33 @@
+main();
+        data.push(Math.floor(Math.random() * 100) + 1);
+    for (let i = 0; i < 10; i++) {
+}
+function generateRandomData() {
+}
+    const data = [];
+    });
+    data.forEach(item => {
+function main() {
+
+
+        console.log("Random Number: " + item);
+    return data;
+    }
+
+    const data = generateRandomData();
+    for (let i = 0; i < 10; i++) {
+function main() {
+    return data;
+    const data = [];
+    data.forEach(item => {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    });
+}
+        console.log("Random Number: " + item);
+    const data = generateRandomData();
+function generateRandomData() {
+}
+
+main();
+
+    }
